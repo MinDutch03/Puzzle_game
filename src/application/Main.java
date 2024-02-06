2cfc2abb1f479730c76fad66ffe38136c9e9cc75
Duc Nguyen <nguyenminhduc890@gmail.com>
2024-02-06 18:47:54 +0100

    final update

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import Scene.SceneManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 
 
@@ -13,8 +14,8 @@ public class Main extends Application {
 		try {
 			SceneManager manager = new SceneManager();
 			primaryStage = manager.getMainStage();
-
-
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("298852_puzzle_icon.png")));
+			primaryStage.setTitle("Puzzle");
 
 			primaryStage.show();
 		} catch(Exception e) {
